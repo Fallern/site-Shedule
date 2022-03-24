84d5e9312afd963ef5095988d99b1d34c4ed68c4
Loginov Alexey <48813221+Fallern@users.noreply.github.com>
2022-03-24 06:40:41 +0300

    refactor: change structure of the project

--- sheduleSite/shedule/urls.py
+++
@@ -1,8 +0,0 @@
-"""The url of the scheme for learning_logs"""
-from django.urls import path
-from . import views
-
-app_name = 'shedule'
-urlpatterns = [
-    path('', views.index, name='index'),
-]
